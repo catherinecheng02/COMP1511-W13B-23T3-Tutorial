28daf169f689682de59a0a08c7736fcf8d3667d1
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-09-13 18:00:59 +1000

    Create face1.c

---
+++ tute01/face1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o\n");
+    printf(" - \n");
+
+    // printf("~ ~\n0 0\n o\n - \n");
+    return 0;
+}
