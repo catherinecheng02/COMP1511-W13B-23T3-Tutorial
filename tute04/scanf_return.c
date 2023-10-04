260450d7c05f6fbd0dfe31d8a6c3d80c5e81d400
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-10-04 14:27:29 +1100

    Create scanf_return.c

---
+++ tute04/scanf_return.c
@@ -0,0 +1,13 @@
+// Scanning fixed number of inputs into array
+// Written by Catherine Cheng (z5310517)
+
+#include <stdio.h>
+int main(void) {
+    // scanf return value
+    int input;
+    int input2;
+    int result = scanf("%d %d", &input, &input2);
+    printf("scanf return val = %d\n", result); // what will this print?
+    
+    
+}
