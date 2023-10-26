c148570496759b6949304dd754ad0b2f9b62735a
catherinecheng02 <z5310517@ad.unsw.edu.au>
2023-10-26 21:15:48 +1100

    Create command_line_args.c

---
+++ tute07/command_line_args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    // int argc – the number of arguments passed into command line.
+    // char *argv[] – array of strings representing all the command line arguments 
+    return 0;
+}
+
+
+// e.g. ./program hello world
+// argc = 3
+// argv = {"./program", "hello", "world"}
+// argv[0] = "./program"
+// argv[1][0] = 'h'
